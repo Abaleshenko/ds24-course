555894236e0c1c93fdcf98da99364d27de1eb236
Your Name <you@example.com>
2023-12-03 15:25:26 +0200

    advanced-mean-start-lecture01

--- 01_tendency.py
+++ 01_tendency.py
@@ -10,3 +10,13 @@ def mean(start: list) -> float:
     return sum(start) / len(start)
 
 # print(mean(collection))
+
+
+#task2
+def find_average(*args) -> float:
+    result = 0
+    for i in args[0]:
+        result += i
+    return result / len(args[0])
+
+# print(find_average(collection))
